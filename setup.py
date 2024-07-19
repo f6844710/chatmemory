d5e0c0a8e63279f723036e8b44dc246bd93ba48b
f6844710 <127287746+f6844710@users.noreply.github.com>
2024-07-19 09:51:32 +0900

    v0.1.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     maintainer="uezo",
     maintainer_email="uezo@uezo.net",
     description="Long-term and medium-term memories between you and chatbot💕",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     install_requires=["fastapi==0.100.0", "openai==1.34.0", "requests==2.31.0", "SQLAlchemy==2.0.20", "uvicorn==0.23.1", "pycryptodome==3.18.0"],
     license="Apache v2",
